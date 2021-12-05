601c982837ed734ee62b8dd79c0c53128367d5ef
MarKs <markus.schuermann@hs-furtwangen.de>
2021-12-05 19:54:57 +0100

    scene save

--- Unity Prototyp/Assets/Weinlager/Scripts/LookAt.cs
+++ Unity Prototyp/Assets/Weinlager/Scripts/LookAt.cs
@@ -6,9 +6,14 @@ public class LookAt : MonoBehaviour
 {
     public Transform Player;
 
+    private void Awake()
+    {
+        Player = GameObject.Find("Player").transform;
+    }
+
     void Update()
     {
         gameObject.transform.LookAt(Player);
-        gameObject.transform.localEulerAngles = new Vector3(0, gameObject.transform.localRotation.x+180, 0);
+        gameObject.transform.localEulerAngles = new Vector3(0, gameObject.transform.localEulerAngles.y + 180, 0);
     }
 }
